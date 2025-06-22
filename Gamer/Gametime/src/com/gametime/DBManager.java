256c2752c2b5aa079cc0629edf119b0ac5517d4f
Kanak Bodkhe <kanakbodkhe@gmail.com>
2025-06-23 01:10:45 +0530

    Add files via upload

---
+++ Gamer/Gametime/src/com/gametime/DBManager.java
@@ -0,0 +1,24 @@
+package com.gametime;
+
+import java.sql.*;
+
+public class DBManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/snakegame";
+    private static final String USER = "root";
+    private static final String PASSWORD = "MySQLBoi@69"; // replace with your MySQL root password
+
+    public static void saveScore(int score) {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            String sql = "INSERT INTO players (score) VALUES (?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, score);
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
